eac3eae11b45324b7c2bc99b1316b079946d6041
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-06-12 15:23:49 -0400

    movement

--- index.js
+++ index.js
@@ -1,44 +1,78 @@
-const canvas = document.getElementById("game");
-const width = window.innerWidth;
-const height = window.innerHeight;
-canvas.width = width;
-canvas.height = height;
-const radius = 50;
-const context = canvas.getContext("2d");
-
-let start;
-let x = width / 2;
-let y = height / 2;
-let dx = 100;
-let dy = 100;
-
-
-function step(timestamp) {
-  if(start === undefined) {
-    start = timestamp;
+class V2 {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
-  const deltaTime = (timestamp - start) * 0.001;
-  start = timestamp;
 
-  if(x + radius >= width || x - radius <= 0) dx *= -1;
-  if(y + radius >= height || y - radius <= 0) dy *= -1;
+  add(v) {
+    return new V2(this.x + v.x, this.y + v.y);
+  }
 
-  x += dx * deltaTime;
-  y += dy * deltaTime;
+  sub(v) {
+    return new V2(this.x - v.x, this.y - v.y);
+  }
 
-  context.clearRect(0, 0, width, height);
-  drawCircle(context, x, y, radius, "blue");
-
-  window.requestAnimationFrame(step);
+  scale(s) {
+    return new V2(this.x * s, this.y * s);
+  }
 }
 
-window.requestAnimationFrame(step);
-
-
-function drawCircle(context, x, y, radius, color) {
+function drawCircle(context, center, radius, color) {
   context.beginPath();
-  context.arc(x, y, radius, 0, 2 * Math.PI);
+  context.arc(center.x, center.y, radius, 0, 2 * Math.PI);
   context.fillStyle = color;
   context.fill();
   context.stroke();
 }
+
+(() => {
+  const canvas = document.getElementById("game");
+  const context = canvas.getContext("2d");
+  const radius = 50;
+  let speed = 800;
+  let start;
+  let pos = new V2(radius + 10, radius + 10);
+  let vel = new V2(0, 0);
+
+  const dirMap = {
+    'KeyW': new V2(0, -speed),
+    'KeyS': new V2(0, speed),
+    'KeyA': new V2(-speed, 0),
+    'KeyD': new V2(speed, 0)
+  };
+
+  function step(timestamp) {
+    if(start === undefined) {
+      start = timestamp;
+    }
+    const deltaTime = (timestamp - start) * 0.001;
+    start = timestamp;
+
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    pos = pos.add(vel.scale(deltaTime));
+
+    context.clearRect(0, 0, width, height);
+    drawCircle(context, pos, radius, "blue");
+
+    window.requestAnimationFrame(step);
+  }
+
+  window.requestAnimationFrame(step);
+
+  document.addEventListener("keydown", event => {
+    if (event.code in dirMap) {
+      vel = vel.add(dirMap[event.code]);
+    }
+  })
+
+  document.addEventListener("keyup", event => {
+    if (event.code in dirMap) {
+      vel = vel.sub(dirMap[event.code]);
+    }
+  })
+})();
+
