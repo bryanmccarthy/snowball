bfd6db9d13ea64b408e84efb88f0c8d3ffa21d2f
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-06-15 11:55:40 -0400

    game class

--- index.js
+++ index.js
@@ -17,6 +17,85 @@ class V2 {
   }
 }
 
+const speed = 800;
+const radius = 50;
+
+const dirMap = {
+  'KeyW': new V2(0, -1.0),
+  'KeyS': new V2(0, 1.0),
+  'KeyA': new V2(-1.0, 0),
+  'KeyD': new V2(1.0, 0)
+};
+
+class TutPopup {
+  constructor() {
+    this.alpha = 0.0;
+    this.dalpha = 0.0;
+  }
+
+  update(dt) {
+    
+  }
+
+  render(context) {
+    
+  }
+
+  fadeIn() {
+    this.dalpha = 1.0;
+    this.alpha = 0.0;
+  }
+
+  fadeOut() {
+    this.dalpha = -1.0;
+    this.alpha = 1.0;
+  }
+}
+
+class Game {
+  constructor(pos) {
+    this.pos = new V2(radius + 10, radius + 10);
+    this.pressedKeys = new Set(); 
+    this.popup = new TutPopup();
+  }
+
+  update(dt) {
+    let vel = new V2(0, 0);
+
+    for (let key of this.pressedKeys) {
+      if (key in dirMap) {
+        vel = vel.add(dirMap[key].scale(speed))
+      }
+    }
+
+    this.pos = this.pos.add(vel.scale(dt));
+
+    this.popup.update(dt);
+  }
+
+  render(context) {
+    const width = context.canvas.width;
+    const height = context.canvas.height;
+    
+    context.clearRect(0, 0, width, height);
+    drawCircle(context, this.pos, radius, "blue");
+
+    this.popup.render(context);
+
+    context.fillStyle = "white";
+    context.font = "100px Arial";
+    context.fillText("WASD to move", 100, 100);
+  }
+
+  keyDown(event) {
+    this.pressedKeys.add(event.code);
+  }
+
+  keyUp(event) {
+    this.pressedKeys.delete(event.code);
+  }
+}
+
 function drawCircle(context, center, radius, color) {
   context.beginPath();
   context.arc(center.x, center.y, radius, 0, 2 * Math.PI);
@@ -28,25 +107,16 @@ function drawCircle(context, center, radius, color) {
 (() => {
   const canvas = document.getElementById("game");
   const context = canvas.getContext("2d");
-  const radius = 50;
-  let speed = 600;
+
+  const game = new Game();
+  
   let start;
-  let pos = new V2(radius + 10, radius + 10);
-
-  const dirMap = {
-    'KeyW': new V2(0, -speed),
-    'KeyS': new V2(0, speed),
-    'KeyA': new V2(-speed, 0),
-    'KeyD': new V2(speed, 0)
-  };
-
-  const pressedKeys = new Set(); 
 
   function step(timestamp) {
     if(start === undefined) {
       start = timestamp;
     }
-    const deltaTime = (timestamp - start) * 0.001;
+    const dt = (timestamp - start) * 0.001;
     start = timestamp;
 
     const width = window.innerWidth;
@@ -54,29 +124,20 @@ function drawCircle(context, center, radius, color) {
     canvas.width = width;
     canvas.height = height;
 
-    let vel = new V2(0, 0);
-    for (let key of pressedKeys) {
-      if (key in dirMap) {
-        vel = vel.add(dirMap[key])
-      }
-    }
-
-    pos = pos.add(vel.scale(deltaTime));
-
-    context.clearRect(0, 0, width, height);
-    drawCircle(context, pos, radius, "blue");
-
+    game.update(dt);
+    game.render(context);
+    
     window.requestAnimationFrame(step);
   }
 
   window.requestAnimationFrame(step);
 
   document.addEventListener("keydown", event => {
-    pressedKeys.add(event.code);
+    game.keyDown(event);
   })
 
   document.addEventListener("keyup", event => {
-    pressedKeys.delete(event.code);
+    game.keyUp(event);
   })
 })();
 
